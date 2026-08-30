2220cb118b319e4b176fd0aec47e869a05e40fa9
Patruxs <Patruxs@users.noreply.github.com>
2026-08-30 11:30:28 +0700

    Add Starship prompt feature
    Implement the `starship_prompt` feature across all platforms. Configure
    it for bash, zsh, and PowerShell, and manage the config file via
    chezmoi to ensure a consistent shell prompt.

--- home/dot_config/powershell/Microsoft.PowerShell_profile.ps1
+++ home/dot_config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,7 @@
 if (Get-Command zoxide -ErrorAction SilentlyContinue) {
     Invoke-Expression (& { (zoxide init powershell | Out-String) })
 }
+
+if (Get-Command starship -ErrorAction SilentlyContinue) {
+    Invoke-Expression (& { (starship init powershell | Out-String) })
+}
