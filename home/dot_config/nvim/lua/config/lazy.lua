5a7b98c06f07bb977e58814746115efba55d8bcc
Patruxs <Patruxs@users.noreply.github.com>
2026-09-17 22:52:56 +0700

    Configure Zed editor and disable lazy.nvim rocks bootstrap

--- home/dot_config/nvim/lua/config/lazy.lua
+++ home/dot_config/nvim/lua/config/lazy.lua
@@ -31,6 +31,10 @@ require("lazy").setup({
     -- version = "*", -- try installing the latest stable version for plugins that support semver
   },
   install = { colorscheme = { "tokyonight", "habamax" } },
+  rocks = {
+    -- no installed plugin requires luarocks, so skip the hererocks bootstrap
+    enabled = false,
+  },
   checker = {
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
